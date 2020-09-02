b7dad54a9add6231c6f86d9550b39482de34f890
Peter Ciccone <50625576+ciccpa@users.noreply.github.com>
2020-09-02 12:28:51 -0400

    Added Application, Controller, and GUI files.
    Contains all UI code for JavaFX portion.

---
+++ Java/ciccone.bcs345.purchases.presentation/src/ciccone/bcs345/purchases/presentation/PurchasesGraphicalUI.java
@@ -0,0 +1,20 @@
+package ciccone.bcs345.purchases.presentation;
+/** 
+ * Launches the Graphical UI.
+ */
+import ciccone.bcs345.purchases.presentation.*;
+
+import javafx.application.Application;
+/**
+ * Creates the Application based on PurchasesApplication class.
+ * 
+ * @param String[] args.
+ * 
+ */
+public class PurchasesGraphicalUI {
+		
+	public static void ShowUI(String[] args)
+	{
+		Application.launch(PurchasesApplication.class, args);
+	}
+}	
